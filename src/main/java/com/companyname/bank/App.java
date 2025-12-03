a7b6c39a867b00634050e424d598d7ec761de77c
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2025-12-03 08:45:42 +0530

    new change for poll scm

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github hook trigger test commit");
+        System.out.println( "github new change for poll scm");
     }
 }

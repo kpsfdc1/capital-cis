589ccfe5fde84ac87ae3c4d10707834a758c84b9
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2025-12-25 10:50:30 +0530

    new change on github repo

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github test commit for  new webhook");
+        System.out.println( "new change on github repo");
     }
 }

5babfca5842d5fa079d88429b722a550e37e20e5
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2026-06-02 08:41:13 +0530

    new code change on github

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "another test commit for webhook");
+        System.out.println( "new code change on github");
     }
 }
